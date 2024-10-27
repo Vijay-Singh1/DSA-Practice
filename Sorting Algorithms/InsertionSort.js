245972e75de3c89814f28be056ee1f49b4c35bec
vijay <vijay8869.vs2@gmail.com>
2024-10-27 13:12:37 +0530

    added insertion sort algorithm

---
+++ Sorting Algorithms/InsertionSort.js
@@ -0,0 +1,18 @@
+const InsertionSort = (arr) =>{
+    for(i=1;i<arr.length;i++){
+
+        const key = arr[i]
+        let j = i-1
+
+        while(j>=0 && key < arr[j]){
+           arr[j+1] = arr[j]
+           j--
+        }
+
+        arr[j+1] = key
+    }
+    return arr
+}
+
+
+console.log(InsertionSort([12, 11, 13, 5, 6]))
